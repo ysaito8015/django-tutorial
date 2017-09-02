522c5fdb01723df6e1a1fb3a3efc59f074dbcdfe
Yusuke Saito <ysaito8015@gmail.com>
2017-09-03 05:07:16 +0900

    wip 404 routing

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,12 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        # ex: /polls/
         url(r'^$', views.index, name='index'),
-        # ex: /polls/5/
-        url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='details'),
-        # ex: /polls/5/results/
-        url(r'^(P<question_id>[0-9]+)/results/$', views.results, name='results'),
-        # ex: /polls/5/vote/
-        url(r'^(P<question_id>[0-9]+)/vote/$', views.vote, name='vore'),
+        url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+        url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+        url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
